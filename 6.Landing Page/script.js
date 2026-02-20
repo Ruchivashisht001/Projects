ef6d17a4c285ecdad97174ece5a6c87dd99ae188
Ruchivashisht001 <ruchigour104@gmail.com>
2026-02-20 13:10:13 +0530

    Fix Changes

--- 6.Landing Page/script.js
+++ 6.Landing Page/script.js
@@ -1,16 +1,35 @@
 const form = document.querySelector(".details");
-const inputs = document.querySelectorAll(".name");
+const inputs = document.querySelectorAll(".field");
 
-form.addEventListener("submit", function(e){
-  e.preventDefault();
-});
-let isvalid = true;
-inputs.forEach(function (input) {
-    if (input.value.trim()=== ""){
-        isvalid = false;
-        input.style.border = "2px solid red";
-    }
-    else{
-       input.style.border = "2px solid green";
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let isvalid = true;
+    inputs.forEach(function (input) {
+        if (input.value.trim() === "") {
+            isvalid = false;
+            input.style.border = "2px solid red";
+        }
+        else {
+            input.style.border = "2px solid green";
+        }
+
+        if (input.type === "email") {
+            if (!isvalidEmail(input.value)) {
+                isvalid = false;
+                alert("Please enter a valud email");
+                input.style.border = "2px solid red";
+            }
+        }
+        if (input.value.trim() === "") {
+            isvalid=false;
+            input.style.border = "2px solid red";
+        } else {
+            input.style.border = "2px solid green";
+        }
+    });
+    if (isvalid) {
+        alert("Form submitted successfully");
+        form.reset();
     }
 });
